1983e733d3be1841ba0caad2d7844a4f8a7c4a42
FarbodDvz <farbod.dvz@gmail.com>
2026-07-10 22:21:09 +0330

    Add new models

---
+++ models/member.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self,id,name,phone,fingerprint_id,join_date):
+        self.name = name
+        self.id = id
+        self.phone = phone
+        self.fingerprint_id = fingerprint_id
+        self.join_date = join_date
---
+++ models/membership.py
@@ -0,0 +1,7 @@
+class Membership:
+    def __init__(self,id,member_id,start_date,end_date,status):
+        self.id = id
+        self.member_id = member_id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.status = status
