1122a049ecf29b85eb56e06c3dae94767802e4c7
FarbodDvz <farbod.dvz@gmail.com>
2026-07-11 14:07:05 +0330

    UI init

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from db import models
+from ui import ui
+import sys
+from PyQt6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     models.create_tables()
+    app = QApplication(sys.argv)
+    window = ui.MainApp()
+    window.show()
+    sys.exit(app.exec())
